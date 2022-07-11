802fcbe770942cede712963a31848e656a6aae62
Jalon Duncan <duncanjalon@gmail.com>
2022-07-11 16:35:37 -0500

    newest project

---
+++ LearnJava/VariableLession/Main.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int firstNumber = 2;
+        int secondNumber = 5;
+        
+    }
+}
